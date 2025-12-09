3d4e2ffeeef52888126380a5a6a5799cbd8be58b
Vahan <ghazarianvahan@gmail.com>
2025-12-09 13:43:23 +0400

    added gitignore and created folder and project

--- folders.tf
+++ folders.tf
@@ -8,8 +8,30 @@ resource "google_folder" "development" {
   parent       = google_folder.application.name
 }
 
+resource "google_folder" "staging" {
+  display_name = var.staging_folder
+  parent       = google_folder.application.name
+}
+
+resource "google_folder" "production" {
+  display_name = var.prod_folder
+  parent       = google_folder.application.name
+}
+
 resource "google_project" "dev_project" {
-  name       = var.project
-  project_id = var.project
+  name       = var.dev_project
+  project_id = var.dev_project
   folder_id  = google_folder.development.name
+}
+
+resource "google_project" "staging_project" {
+  name       = var.staging_project
+  project_id = var.staging_project
+  folder_id  = google_folder.staging.name
+}
+
+resource "google_project" "prod_project" {
+  name       = var.prod_project
+  project_id = var.prod_project
+  folder_id  = google_folder.production.name
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,31 @@
-variable "project" {
+variable "dev_project" {
   default = "ashva-dev"
+}
+
+variable "staging_project" {
+  default = "ashva-staging"
+}
+
+variable "prod_project" {
+  default = "ashva-production"
 }
 
 variable "dev_folder" {
   default = "development"
 }
 
+variable "staging_folder" {
+  default = "staging"
+}
+
+variable "prod_folder" {
+  default = "production"
+}
+
+
+
 variable "folder" {
-  default = "application"
+  default = "applications"
 }
 
 variable "region" {
